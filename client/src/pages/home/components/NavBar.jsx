e6e0a9a69f7f28abf47db9755fb34af4fc8689a3
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-04-15 01:40:57 -0600

    Frontend development 15-04-24

--- client/src/pages/home/components/NavBar.jsx
+++ client/src/pages/home/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
           </NavLink>
         </li>
       </ul>
-      <ul>
+      <ul className="home-buttons-container">
         <li>
           <NavLink
             to="#"
