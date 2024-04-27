fafab9235db5505b59636f69a0932881898f7270
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-04-26 21:50:40 -0600

    mdapdjpasjdo

--- client/src/other/extras.js
+++ client/src/other/extras.js
@@ -1,13 +1,3 @@
-const baseUrl = 'http://localhost:3002';
+const baseUrl = "http://localhost:3002";
 
-const addToCart = (id) => {
-  if(!window.localStorage.getItem('cart')){
-    window.localStorage.setItem('cart', JSON.stringify([]));
-  }else {
-    let cart = JSON.parse(window.localStorage.getItem('cart'));
-    cart.push(id);
-    window.localStorage.setItem('cart', JSON.stringify(cart));
-  }
-} 
-
-module.exports = { baseUrl, addToCart}
+module.exports = { baseUrl };
--- client/src/pages/client/components/Product.jsx
+++ client/src/pages/client/components/Product.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
-import { baseUrl } from "../../../other/extras";
+import { addToCart, baseUrl } from "../../../other/extras";
 
 const Producto = () => {
   const props = useParams();
-  const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState({});
   const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
@@ -22,19 +22,40 @@ const Producto = () => {
       .catch((error) => {
         setError(error.message);
       });
-  }, []);
+  }, [props]);
+
+  const getCart = () => {
+    const cart = window.localStorage.getItem("cart");
+    return cart ? JSON.parse(cart) : [];
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const cart = getCart();
+    console.log(cart);
+    cart.push({ id: data.id });
+    return window.localStorage.setItem("cart", JSON.stringify(cart));
+  };
 
   return (
     <div>
-      {error && <p>{error.message}</p> }
+      {error && <p>{error.message}</p>}
       <div className="id">id : {data.id}</div>
       <div className="nombre">Nombre : {data.nombre}</div>
       <div className="descripcion">Descripcion : {data.descripcion}</div>
       <div className="costo">Costo : ${data.costo}</div>
-      <div className="disponibilidad">Disponibilidad Aproximada : {data.disponibilidad}</div>
+      <div className="disponibilidad">
+        Disponibilidad Aproximada : {data.disponibilidad}
+      </div>
       <div className="estado">{data.estado}</div>
       <div className="id-Cafeteria">ID - Cafeteria{data.id_cafeteria}</div>
-      <div className="tiempo-preparacion">Tiempo de preparacion : {data.tiempo_preparacion} min</div>
+      <div className="tiempo-preparacion">
+        Tiempo de preparacion : {data.tiempo_preparacion} min
+      </div>
+      <button onClick={handleChange}>Add </button>
+      <button onClick={() => window.localStorage.setItem("cart", [])}>
+        Reload
+      </button>
     </div>
   );
 };
