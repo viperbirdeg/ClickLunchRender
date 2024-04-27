6b9f564af1406102967aeeba57ba328a87b46146
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-04-27 17:18:20 -0600

    base Url added

--- client/src/other/extras.js
+++ client/src/other/extras.js
@@ -1,4 +1,6 @@
-const baseUrl = "http://localhost:3002";
+const dotenv = require('dotenv').config;
+
+const baseUrl = process.env.BASE_URL || "http://localhost:3002";
 
 const getCart = () => {
   const cart = window.localStorage.getItem("cart");
