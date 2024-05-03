dd7fd8f8c06db68414a6d754297313e3a1230141
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-05-02 20:29:22 -0600

    Added img options

--- client/src/pages/cafe/components/HomeCafe.jsx
+++ client/src/pages/cafe/components/HomeCafe.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { baseUrl } from "../../../other/extras";
 
-const HomeCafe = () => {
+function HomeCafe() {
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [res, setRes] = useState({});
+  const handleSelectFile = (e) => setFile(e.target.files[0]);
+  const handleUpload = async () => {
+    try {
+      setLoading(true);
+      const data = new FormData();
+      data.append("my_file", file);
+      const res = await axios.post(`${baseUrl}/upload`, data);
+      setRes(res.data);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>HomeCafe</div>
-  )
+    <div className="App">
+      <label htmlFor="file" className="btn-grey">
+        {" "}
+        select file
+      </label>
+      {file && <center> {file.name}</center>}
+      <img src="http://res.cloudinary.com/dxm9ighpe/image/upload/v1714703241/yptesbbm091a93fokptx.png"/>
+      <input
+        id="file"
+        type="file"
+        onChange={handleSelectFile}
+        multiple={false}
+      />
+      <code>
+        {Object.keys(res).length > 0
+          ? Object.keys(res).map((key) => (
+              <p className="output-item" key={key}>
+                <span>{key}:</span>
+                <span>
+                  {typeof res[key] === "object" ? "object" : res[key]}
+                </span>
+              </p>
+            ))
+          : null}
+      </code>
+      {file && (
+        <>
+          <button onClick={handleUpload} className="btn-green">
+            {loading ? "uploading..." : "upload to cloudinary"}
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default HomeCafe
+export default HomeCafe;
--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const session = require("express-session");
 const path = require("path");
 const cors = require("cors");
+const Multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 
 //?Routers
 const app = express();
@@ -42,11 +44,40 @@ app.use(
     },
   })
 );
+//*Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+async function handleUpload(file) {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+}
+//*multer
+const storage = new Multer.memoryStorage();
+const upload = Multer({
+  storage,
+});
+
+
 
 //?Ruta de "apoyo" {Eliminar para produccion final}
 
-app.get("/api", (req, res) => {
-  res.json({ message: "La ptm el servidor no jala" });
+app.post("/upload", upload.single("my_file"), async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
+  }
 });
 
 //?Rutas de api
