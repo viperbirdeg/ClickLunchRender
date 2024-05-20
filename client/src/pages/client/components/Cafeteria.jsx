8673b6b54c04e5b19f2d7d202c306456ad6da0de
Gus-Valdivia <148442038+Gus-Valdivia@users.noreply.github.com>
2024-05-19 22:48:19 -0600

    Cafeterias, Productos

--- client/src/pages/client/components/Cafeteria.jsx
+++ client/src/pages/client/components/Cafeteria.jsx
@@ -8,11 +8,16 @@ const Cafeteria = ({ id, nombre, email }) => {
   return (
     <div className="cafeteria">
       <div>ID : {id}</div>
-      <div>Nombre : {nombre}</div>
-      <div>Contacto : {email}</div>
-      <Link to={route} className="cafeteria-link">
-        Seleccionar
-      </Link>
+        <svg className="svg_neg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M160-720v-80h640v80H160Zm0 560v-240h-40v-80l40-200h640l40 200v80h-40v240h-80v-240H560v240H160Zm80-80h240v-160H240v160Zm-38-240h556-556Zm0 0h556l-24-120H226l-24 120Z" /></svg>
+      <div className="caf_cont">
+        <section className="data_caf">
+          <div className="nombre_caf">{nombre}</div>
+          <div className="cont_caf">{email}</div>
+          <Link to={route} className="cafeteria-link">
+            Seleccionar
+          </Link>
+        </section>
+      </div>
     </div>
   );
 };
