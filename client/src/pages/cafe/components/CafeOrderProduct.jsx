4c2fc455df1755b7bbd4e4aa096da4c58e6c7e2b
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-06-03 15:24:28 -0600

    idk

--- client/src/pages/cafe/components/CafeOrderProduct.jsx
+++ client/src/pages/cafe/components/CafeOrderProduct.jsx
@@ -8,6 +8,7 @@ const CafeOrderProduct = () => {
   const [data, setData] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [elemenstData, setELementsData] = React.useState(null);
 
   React.useEffect(() => {
     axios
@@ -17,22 +18,43 @@ const CafeOrderProduct = () => {
         },
       })
       .then((response) => {
-        console.log(response)
-        setData(response.data.message);
+        console.log(response);
+        setData(response.data.message[0]);
         setIsLoading(false);
       })
       .catch((error) => {
         console.log(error);
         setError(error.message);
       });
+    axios
+      .post(`${baseUrl}/api/pedido/getElementosPedido`, {
+        data: {
+          idPedido: props.id,
+        },
+      })
+      .then(
+        (response) => {
+          setELementsData(response.data.message);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+      .catch((error) => {
+        console.log(error);
+      });
   }, [props]);
 
   return (
     <div>
-    {isLoading ? (<h1>Informacion cargando</h1>) : <div></div>}
+      {isLoading ? <h1>Informacion cargando</h1> : <div></div>}
       <h1>CafeOrderProduct</h1>
       <h2>id: {props.id}</h2>
+      <ul>
+        <li></li>
+      </ul>
       <h3>data: {JSON.stringify(data)}</h3>
+      <h3>Elements data : {JSON.stringify(elemenstData)}</h3>
       {error && <h2>error: {error}</h2>}
       <h2>isLoading: {isLoading}</h2>
     </div>
