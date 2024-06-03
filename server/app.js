314fabedeafe95e61fe7ccee20aebe368ef3427f
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-06-03 16:26:57 -0600

    idkx3

--- server/app.js
+++ server/app.js
@@ -28,18 +28,9 @@ const whitelist = [
   "http://localhost:3000",
   "https://clicklunchrender.onrender.com"
 ];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: whitelist;
+}));
 
 //*Utiliar body parser
 app.use(bodyParser.json());
