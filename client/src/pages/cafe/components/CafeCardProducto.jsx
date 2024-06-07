dd84cddb6dd7a0f0c44e579dac0a2df74a9422eb
Gus-Valdivia <148442038+Gus-Valdivia@users.noreply.github.com>
2024-06-07 06:31:36 -0600

    Cambios uwu

--- client/src/pages/cafe/components/CafeCardProducto.jsx
+++ client/src/pages/cafe/components/CafeCardProducto.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "../../client/css/CardProducto.css"
+import fond_pro from "../../../imagenes/fondotarj5.jpg"
 
 const CafeCardProducto = ({ id, nombre, costo, disponibilidad, url }) => {
   const route = `/cafe/editproduct/${id}`;
 
   return (
     <div className="card-producto">
-      <div>
-        {" "}
-        <img src={url} alt="Imagen no disponible" />
-      </div>
-      <div>ID : {id}</div>
-      <div>Nombre : {nombre} </div>
-      <div>Costo : ${costo} </div>
-      <div> Disponibilidad aproximada : {disponibilidad} productos</div>
-      <Link to={route} className="card-producto-link">
-        Editar
-      </Link>
+      <img className="img_pro_bg" src={fond_pro} alt="Imagen no disponible" />
+      <section className="info_capr">
+        <section>
+          <div className="name_procard">{nombre} </div>
+          <div>Disponibilidad: {disponibilidad}</div>
+        </section>
+        <div className="pre_procard"><span>${costo}</span></div>
+      </section>
+      <img className="img_pro_card" src={url} alt="Imagen no disponible" />
+      <section className="btns_capr" id="btn_edit_pro">
+        <Link to={route} className="card-producto-link">
+          Editar <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+          </svg>
+        </Link>
+      </section>
     </div>
   );
 };
--- client/src/pages/cafe/components/Orders.jsx
+++ client/src/pages/cafe/components/Orders.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { baseUrl } from "../../../other/extras";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
+import bloc from "../../../imagenes/bloc.png";
+import note from "../../../imagenes/note.png";
 
 const Orders = () => {
   const [data, setData] = React.useState([]);
@@ -25,32 +27,33 @@ const Orders = () => {
   }, []);
 
   return (
-    <div className="orders">
-      <h1 className="orders-header">Pedidos en espera</h1>
+    <div className="orders" id="orders_cafe">
+      <h1 className="orders-header">Pedidos</h1>
       <div className="orders-list">
         {data ? (
-          <div>
-            <div>
-              <h1>Pedidos en espera</h1>
+          <div className="list_order_pedidos">
+            <div className="pedido">
+              <h2>Pedido(s) en espera</h2>
               {data.map(
                 (item, index) =>
                   item.estado === "En espera" && (
-                    <ul>
-                      <li>pedido : {item.id_cafeteria * 1000000 + item.id}</li>
-                      <li>id : {item.id}</li>
-                      <li>fecha :{item.fecha_pedido.split("T")[0]}</li>
-                      <li>hora : {item.hora}</li>
-                      <li>estado : {item.estado}</li>
-                      <li>costo : {item.costo_total}</li>
-                      <NavLink to={`/cafe/order/${item.id}`}>
-                        Atender orden
-                      </NavLink>
-                      <hr />
-                    </ul>
+                    <div className="ped_esp">
+                      <img src={bloc} className="img_bloc"/>
+                      <img src={note} className="img_note"/>
+                      <div>
+                        <section className="id_pedido">#{item.id}</section>
+                        <section className="fech_pedido">Pedido efectuado el: {item.fecha_pedido.split("T")[0]} a las {item.hora}</section>
+                        <section className="est_pedido">{item.estado}</section>
+                        <section>${item.costo_total}</section>
+                        <NavLink to={`/cafe/order/${item.id}`}>
+                          Atender orden
+                        </NavLink>
+                      </div>
+                    </div>
                   )
               )}
             </div>
-            <div>
+            <div className="pedido">
               <h1>Pedidos procesandoce</h1>
               {data.map(
                 (item, index) =>
@@ -70,7 +73,7 @@ const Orders = () => {
                   )
               )}
             </div>
-            <div>
+            <div className="pedido">
               <h1>Pedidos Cancelados</h1>
               {data.map(
                 (item, index) =>
@@ -90,7 +93,7 @@ const Orders = () => {
                   )
               )}
             </div>
-            <div>
+            <div className="pedido">
               <h1>Pedidos Finalizados </h1>
               {data.map(
                 (item, index) =>
