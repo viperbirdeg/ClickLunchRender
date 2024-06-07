49b7b10f4b881255ca6cdfaf5738a26dcbb6d1b0
Gus-Valdivia <148442038+Gus-Valdivia@users.noreply.github.com>
2024-06-07 03:06:10 -0600

    Añadir Producto

--- client/src/pages/cafe/components/NewProduct.jsx
+++ client/src/pages/cafe/components/NewProduct.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { baseUrl } from "../../../other/extras";
 import axios from "axios";
 import '../css/NewProduct.css'
+import caf from "../../../imagenes/morales.png"
 import { soloLetrasNormales, soloNumeros } from "../../../other/validation";
 
 const NewProduct = () => {
@@ -16,6 +17,8 @@ const NewProduct = () => {
   const [imagina, setImagina] = React.useState(null);
   const [res, setRes] = React.useState(null);
 
+  const fileInputRef = React.useRef(null);
+
   const handleChangeNumerico = (e) => {
     soloNumeros(e);
     setCredentials({
@@ -27,10 +30,10 @@ const NewProduct = () => {
   const handleChange = (e) => {
     soloLetrasNormales(e);
     setCredentials({
-     ...credentials,
+      ...credentials,
       [e.target.name]: e.target.value,
     });
-  }
+  };
 
   React.useEffect(() => {
     if (imagina) {
@@ -86,72 +89,103 @@ const NewProduct = () => {
         alert("No se ha podido añadir la imagen");
       });
   };
+
+  const handleSvgClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
-    <div>
-      Agregar alimento
-      <form onSubmit={handleSubmit}>
-        <div className="input-form-container">
-          <input
-            id="file"
-            type="file"
-            accept="image/png, image/jpeg, image/jpg"
-            onChange={handleSelectFile}
-            multiple={false}
-          />
-          {imagina && (
-            <div className="img-container">
-              <img src={imagina} alt="" />
-            </div>
-          )}
-        </div>
-        <div className="input-form-contaienr">
-          Nombre :{" "}
-          <input
-            type="text"
-            name="nombre"
-            placeholder="Nombre"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="input-form-contaienr">
-          Descripcion :{" "}
-          <input
-            type="text"
-            name="descripcion"
-            placeholder="Descripcion"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="input-form-contaienr">
-          Tiempo de preparacion en minutos :{" "}
-          <input
-            type="text"
-            name="tiempopreparacion"
-            placeholder="Tiempo Preparacion"
-            onChange={handleChangeNumerico}
-          />
-        </div>
-        <div className="input-form-contaienr">
-          Precio en pesos mexicanos :{" "}
-          <input
-            type="text"
-            name="costo"
-            placeholder="Precio"
-            onChange={handleChangeNumerico}
-          />
-        </div>
-        <div className="input-form-contaienr">
-          Disponibilidad aproximada :{" "}
-          <input
-            type="text"
-            name="disponibilidad"
-            placeholder="Disponibilidad"
-            onChange={handleChangeNumerico}
-          />
-        </div>
-        <div className="input-form-contaienr">
-          <input type="submit" value="Agregar" />
-        </div>
+    <div className="producto_cont" id="producto_contAA">
+      <form onSubmit={handleSubmit} className="producto">
+        <section className="img_cont_pro">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            className="bi bi-image"
+            viewBox="0 0 16 16"
+            onClick={handleSvgClick}
+          >
+            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z" />
+          </svg>
+          <div className="input-form-container">
+            <input
+              id="file"
+              type="file"
+              accept="image/png, image/jpeg, image/jpg"
+              onChange={handleSelectFile}
+              ref={fileInputRef}
+              style={{ display: 'none' }}
+              multiple={false}
+            />
+            {imagina && (
+              <div className="img-container" id="img_cont_agrpro">
+                <img src={imagina} alt="" />
+              </div>
+            )}
+          </div>
+        </section>
+        <section className="text_desc_pro">
+          <img className="fond_pro" src={caf} alt="Imagen no encontrada" />
+          <div className="input-form-contaienr">
+            Nombre del Nuevo Producto:{" "}
+            <input
+              type="text"
+              name="nombre"
+              placeholder="Nombre"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-form-contaienr">
+            Agregue una descripsión de su producto:{" "}
+            <input
+              type="text"
+              name="descripcion"
+              placeholder="Descripcion"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-form-contaienr">
+            Tiempo de preparacion en minutos (Solo Números):{" "}
+            <section>
+              <input
+                type="text"
+                name="tiempopreparacion"
+                placeholder="Tiempo Preparacion"
+                onChange={handleChangeNumerico}
+              />
+              min
+            </section>
+          </div>
+          <div className="input-form-contaienr">
+            Precio del producto en pesos mexicanos (Solo Números):{" "}
+            <section>
+              $
+              <input
+                type="text"
+                name="costo"
+                placeholder="Precio"
+                onChange={handleChangeNumerico}
+              />
+            </section>
+          </div>
+          <div className="input-form-contaienr">
+            Disponibilidad aproximada en unidades (Solo Números):{" "}
+            <section>
+              <input
+                type="text"
+                name="disponibilidad"
+                placeholder="Disponibilidad"
+                onChange={handleChangeNumerico}
+              />
+              Unidades.
+            </section>
+          </div>
+          <div className="input-form-contaienr" id="inp_btn">
+            <input type="submit" value="Agregar nuevo Producto" />
+          </div>
+        </section>
       </form>
     </div>
   );
