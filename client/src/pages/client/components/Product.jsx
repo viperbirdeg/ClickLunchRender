d00694d178f97fff9c87ba90c28d962054fc28ae
viperbirdeg <76213733+viperbirdeg@users.noreply.github.com>
2024-06-13 21:46:08 -0600

    FIX PLIS

--- client/src/pages/client/components/Product.jsx
+++ client/src/pages/client/components/Product.jsx
@@ -11,15 +11,31 @@ const Producto = () => {
   const [data, setData] = React.useState({});
   const [error, setError] = React.useState(null);
   const [value, setValue] = useState(1);
+
+  const solo10 = (e) => {
+    e.preventDefault();
+    let value = e.target.value;
+    e.target.value = value.replace(/^[0-9]{0,2}$/g, "");
+  };
+
+  const handleChande = (e) => {
+    solo10(e);
+    setValue(Number(e.target.value));
+  };
+
   const decrementValue = () => {
-    if (value === 1) {
+    if (value <= 1) {
       setValue(1);
     } else {
       setValue(value - 1);
     }
   };
   const incrementValue = () => {
-    setValue(value + 1);
+    if (value >= 10) {
+      setValue(10);
+    } else {
+      setValue(value + 1);
+    }
   };
 
   React.useEffect(() => {
@@ -40,12 +56,12 @@ const Producto = () => {
   const handleChange = (e) => {
     e.preventDefault();
     const cart = getCart();
-    for(let i = 0; i < value; i++){
+    for (let i = 0; i < value; i++) {
       cart.push({ id: data.id });
     }
-    if(value === 1){
+    if (value === 1) {
       alert("Producto agregado");
-    }else{
+    } else {
       alert("Productos agregado");
     }
     return window.localStorage.setItem("cart", JSON.stringify(cart));
@@ -74,25 +90,62 @@ const Producto = () => {
           <hr />
           <div className="descripcion">{data.descripcion}</div>
           <hr />
-          <div className="tiempo-preparacion">Tiempo de preparacion : {data.tiempo_preparacion} min</div>
-          <div className="disponibilidad">Unidades Disponibles: {data.disponibilidad}</div>
+          <div className="tiempo-preparacion">
+            Tiempo de preparacion : {data.tiempo_preparacion} min
+          </div>
+          <div className="disponibilidad">
+            Unidades Disponibles: {data.disponibilidad}
+          </div>
           <hr />
           <div className="estado">{data.estado}</div>
           <div className="opc_product">
             <section className="opc_product_btns">
               <section className="cant_cont">
                 <span>Cantidad: </span>
-                <input type="number" name="" id="" value={value} onChange={(e) => setValue(Number(e.target.value))} />
-                <svg onClick={decrementValue} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                <input
+                  type="number"
+                  name=""
+                  id=""
+                  value={value}
+                  onChange={handleChande}
+                  disabled={true}
+                />
+                <svg
+                  onClick={decrementValue}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-dash-circle-fill"
+                  viewBox="0 0 16 16"
+                >
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
                 </svg>
-                <svg onClick={incrementValue} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <svg
+                  onClick={incrementValue}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-plus-circle-fill"
+                  viewBox="0 0 16 16"
+                >
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                 </svg>
               </section>
-              <button onClick={handleChange}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-              </svg> Añadir al Carrito</button>
+              <button onClick={handleChange}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-cart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                </svg>{" "}
+                Añadir al Carrito
+              </button>
             </section>
             <div className="costo">${data.costo}</div>
           </div>
